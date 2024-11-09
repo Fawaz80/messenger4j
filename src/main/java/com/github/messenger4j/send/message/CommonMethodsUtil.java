6cf2dca91024b43331f75c2d56089e32f2e1fc41
Fawaz80 <133235052+Fawaz80@users.noreply.github.com>
2024-11-09 18:17:41 +0300

    Create CommonMethodsUtil.java

---
+++ src/main/java/com/github/messenger4j/send/message/CommonMethodsUtil.java
@@ -0,0 +1,25 @@
+import java.net.URL;
+import java.util.Optional;
+
+public class CommonMethodsUtil {
+
+    public static URL getUrl(URL url) {
+        return url;
+    }
+
+    public static Optional<WebviewHeightRatio> getWebviewHeightRatio(Optional<WebviewHeightRatio> webviewHeightRatio) {
+        return webviewHeightRatio;
+    }
+
+    public static Optional<Boolean> getMessengerExtensions(Optional<Boolean> messengerExtensions) {
+        return messengerExtensions;
+    }
+
+    public static Optional<URL> getFallbackUrl(Optional<URL> fallbackUrl) {
+        return fallbackUrl;
+    }
+
+    public static Optional<WebviewShareButtonState> getWebviewShareButtonState(Optional<WebviewShareButtonState> webviewShareButtonState) {
+        return webviewShareButtonState;
+    }
+}
